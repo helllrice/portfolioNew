526007164e0865ee1ca9cad9a079c4c192ba077a
Vasiliy <63386773+helllrice@users.noreply.github.com>
2020-10-02 14:22:17 +0300

    skills

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,42 @@
-$(document).ready(function(){
+const IMAGES = [ 
+    'src/assets/image/123.jpg',
+    'src/assets/image/123.jpg',
+    'src/assets/image/123.jpg',
+    'src/assets/image/123.jpg',
+    'src/assets/image/123.jpg'
+   ]
 
+$(document).ready(function(){
+    $('#my-slider').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        arrows: true,
+        slidesToScroll: 1,
+        centerPadding: '0px',
+        prevArrow: '<button class="my-slider__arrow">назад</button>',
+        nextArrow: '<button class="my-slider__arrow">вперед</button>',
+        adaptiveHeight: true
+    });
+
+    IMAGES.forEach(function(item) {
+        const html = `
+        <div class="my-slider__item">
+            <img src="${ item } " alt="" class="my-slider__img">
+            <div class="my-slider__hidden">
+                <a href="#">подробнее</a>
+            </div>
+        </div>
+        `
+        $('#my-slider').slick('slickAdd', html);
+        console.log(html)
+    });
 })
+
+
+const progress = document.querySelector('.progress-done');
+
+
+setTimeout(() => {
+    progress.style.width = progress.getAttribute('data-done') + '%';
+    progress.style.opacity = 1;
+}, 500);
